fd0434511ef8f648f153556b828cf97448916349
Mikias Gedlu <addiserp@gmail.com>
2024-04-08 17:31:53 +0300

    0x03-Unittests_and_integration_tests 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -31,3 +31,20 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(google_client.org(), expected_res)
         mock_function.assert_called_once_with("https://api.github.com/orgs/{}"
                                               .format(org))
+
+    def test_public_repos_url(self):
+        """
+            test_public_repos_url method to unit-test
+            GithubOrgClient._public_repos_url.
+        """
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock
+        ) as mock_property:
+            mock_property.return_value = {
+                "repos_url": "https://api.github.com/users/google/repos"
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos"
+            )
